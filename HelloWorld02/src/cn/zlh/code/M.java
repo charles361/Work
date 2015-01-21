c1b78892ca12cee42f27f219be92118d90d382fd
charles361 <skysea361@163.com>
2015-01-21 15:11:51 +0800

    ccc

---
+++ HelloWorld02/src/cn/zlh/code/M.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package cn.zlh.code;
+
+/**           
+ * 创建人：朱林虎    
+ * QQ:279562102
+ * Email:skysea361@163.com
+ * 创建时间：2015-1-21 下午03:11:39      
+ */
+public class M {
+
+	/**
+	 * 
+	 */
+	public M() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
